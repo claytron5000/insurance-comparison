3cf5b61167570d161c9dc22e8c8c3ec8d2068dd7
Clayton Salem <claytonjsalem@gmail.com>
2018-12-02 21:58:55 -0500

    Move back to docs

---
+++ docs/precache-manifest.581c3e964a9a9b236bc2a85a888f8bd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44e2d52ef93bedc10b7b",
+    "url": "/insurance-comparison/static/js/runtime~main.44e2d52e.js"
+  },
+  {
+    "revision": "7e82d5ae57ca0f7b8a65",
+    "url": "/insurance-comparison/static/js/main.7e82d5ae.chunk.js"
+  },
+  {
+    "revision": "22d3a9e84b680f412c3a",
+    "url": "/insurance-comparison/static/js/1.22d3a9e8.chunk.js"
+  },
+  {
+    "revision": "7e82d5ae57ca0f7b8a65",
+    "url": "/insurance-comparison/static/css/main.e615cc2b.chunk.css"
+  },
+  {
+    "revision": "0b0cd55f012ecb0bff1b7d9f7c3d1792",
+    "url": "/insurance-comparison/index.html"
+  }
+];
